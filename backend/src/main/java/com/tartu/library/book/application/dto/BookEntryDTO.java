cea89df6077da9f207b6dc111fafa86e4f82c3cf
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-05-18 08:22:13 +0300

    add assembler

--- backend/src/main/java/com/tartu/library/book/application/dto/BookEntryDTO.java
+++ backend/src/main/java/com/tartu/library/book/application/dto/BookEntryDTO.java
@@ -1,11 +1,12 @@
 package com.tartu.library.book.application.dto;
 
 import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.time.LocalDate;
 
 @Data
-public class BookEntryDTO {
+public class BookEntryDTO extends RepresentationModel<BookEntryDTO> {
     String ISBN;
     String bookName;
     String author;
---
+++ backend/src/main/java/com/tartu/library/book/application/services/BookAssembler.java
@@ -0,0 +1,24 @@
+package com.tartu.library.book.application.services;
+
+import com.tartu.library.book.application.dto.BookEntryDTO;
+import com.tartu.library.book.domain.model.BookEntry;
+import com.tartu.library.book.rest.BookRestController;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookAssembler extends RepresentationModelAssemblerSupport<BookEntry, BookEntryDTO> {
+    public BookAssembler() {
+        super(BookRestController.class, BookEntryDTO.class);
+    }
+
+    @Override
+    public BookEntryDTO toModel(BookEntry bookEntry) {
+        BookEntryDTO dto = createModelWithId(bookEntry.getId(), bookEntry);
+        dto.setAuthor(bookEntry.getAuthor());
+        dto.setBookName(bookEntry.getBookName());
+        dto.setPublishDate(bookEntry.getPublishDate());
+        dto.setISBN(bookEntry.getISBN());
+        return dto;
+    }
+}
