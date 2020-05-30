a4dafbcd6b93fba4cd0d43464b3160643425b65b
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-05-30 23:20:00 +0300

    add template api's

--- backend/src/main/java/com/tartu/library/book/rest/BookEntryRestController.java
+++ backend/src/main/java/com/tartu/library/book/rest/BookEntryRestController.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,5 +37,17 @@ public class BookEntryRestController {
   public BookEntryDTO retrieveBook(@PathVariable UUID uuid) {
     logger.info(String.format("Retrieving Book Entry (%s)", uuid.toString()));
     return bookService.retrieveBookEntryDTO(uuid);
+  }
+
+  @PatchMapping("{uuid}")
+  public ResponseEntity<BookEntryDTO> modifyBook(@PathVariable UUID uuid) {
+    logger.info(String.format("Modifying Book Entry (%s)", uuid.toString()));
+    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+  }
+
+  @DeleteMapping("{uuid}")
+  public ResponseEntity<BookEntryDTO> deleteBook(@PathVariable UUID uuid) {
+    logger.info(String.format("Deleting Book Entry (%s)", uuid.toString()));
+    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
   }
 }
