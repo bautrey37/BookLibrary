da86f7fae910940e40120262d7c705cd8142e31e
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-05-31 17:37:24 +0300

    update import

--- backend/src/main/java/com/tartu/library/person/domain/repository/PersonRepository.java
+++ backend/src/main/java/com/tartu/library/person/domain/repository/PersonRepository.java
@@ -1,6 +1,5 @@
 package com.tartu.library.person.domain.repository;
 
-import com.tartu.library.book.domain.model.BookEntry;
 import com.tartu.library.person.domain.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
