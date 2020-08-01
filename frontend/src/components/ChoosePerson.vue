9a2238183367c87bc7b5110f5382d72fcbe803e5
Brandon <brandon.c.autrey@gmail.com>
2020-08-01 15:53:18 +0300

    add new book page with person chooser

---
+++ frontend/src/components/ChoosePerson.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="person in persons" :key="person">{{ person }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Person.vue',
+  data() {
+    return {
+      persons: ['Brandon', 'Rain', 'Oleh'],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ frontend/src/views/AddNewBook.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+    <form>
+      <b-field name="Book Name">
+        <b-input v-model="formData.bookInfo.bookName"></b-input>
+      </b-field>
+      <choose-person></choose-person>
+    </form>
+    <b-button @click.prevent="submit">Submit</b-button>
+  </div>
+</template>
+
+<script>
+import ChoosePerson from '@/components/ChoosePerson';
+export default {
+  name: 'AddNewBook.vue',
+  data() {
+    return {
+      formData: {
+        serialNumber: '',
+        bookInfo: {
+          bookName: '',
+          author: '',
+          publishDate: '',
+        },
+      },
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.formData);
+    },
+  },
+  components: {
+    ChoosePerson,
+  },
+};
+</script>
+
+<style scoped></style>
